e8f67388563a4b4833f796620724bf67a4b5df86
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-16 19:22:16 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version 
+
+//alert(3 +3) // We are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readliblity should be high
+
+console.log("Pranita")
+
+let name = "Pranita"
+let age = 18
+let isLoggedIn = false;
+let str;
+
+// Datatype in js
+//number => 2 to power 53
+//bigint for large range data
+//string => "" it may be in single or double quotes
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//object
+
+console.log(typeof "Pranita")
+console.log(typeof age)
+console.log(typeof undefined); //undefined is one datatype
+console.log(typeof str)
+console.log(typeof null) //object
