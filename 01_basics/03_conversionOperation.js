45042c554379deff995450e98a7c5c8b78e257b4
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-16 19:42:27 +0000

    conver_operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) // NaN means what Not a Number
+
+
