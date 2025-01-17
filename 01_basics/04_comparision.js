76dc0156e3f8bb745546d6822c22a6a76d09ce95
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-17 14:00:52 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -4,4 +4,28 @@
 // console.log(2 == 1); //false
 // console.log(2 != 1); //true
 
+console.log("2" > 1);//t
+console.log("02" > 1);//t
+
+
+
+// equality == and comparision >,<,>=,<= works differently.Comparision convert a null into 0 and that why 3rd is true and 1st is false
+// console.log(null > 0); //false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true
+
+// console.log(undefined == 0);//f
+// console.log(undefined > 0);//f
+// console.log(undefined < 0);//f
+
+// === => checks the datatype and not allow to convert them.It checks strictly.
+console.log("2" === 2);//false.str and number
+
+
+
+
+
+
+
+
 
