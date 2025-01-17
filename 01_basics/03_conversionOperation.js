dd59d39dc5fd8f96e8b50dae4ee489a18fb264e9
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-17 06:41:15 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,41 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score)) //another way to find typeof variable
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber) // NaN means what Not a Number
+// let valueInNumber = Number(score) //score is converted into number datatype
+// console.log(typeof valueInNumber) //number
+// console.log(valueInNumber) // NaN means what Not a Number
+
+// let score1 = null;
+
+// let valueInNumber1 = Number(score1) 
+// console.log(typeof valueInNumber1) 
+// console.log(valueInNumber1) // null is converted into 0.
+
+// let score2 = undefined;
+// let valueInNumber2 = Number(score2) 
+// console.log(typeof valueInNumber2) 
+// console.log(valueInNumber2) //Nan
+
+//"33"=> 33
+//"33abc" => Nan (number)
+//true => 1
+//false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false;
+// "" => false
+// "hitesh" => true
+
+let somenum = 33;
+let stringnum = String(somenum);
+console.log(stringnum);
+console.log(typeof stringnum);
+
 
 
