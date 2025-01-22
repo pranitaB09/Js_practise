733660f3298edf43492b33e616caea7960d7138f
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-22 16:58:03 +0000

    primitive_objects

---
+++ 01_basics/primitive_and_objects.js
@@ -0,0 +1,12 @@
+/**
+ * N-Null
+ * N-Number
+ * S-Symbol
+ * S-String
+ * B-Boolean
+ * B-Bigint
+ * U-Undefined
+ */
+//Datatype-means what type of data is strored in a variable like number,string
+//Object:key-value pairs 
+
