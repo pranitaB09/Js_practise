40b0722951c6f3d0571862632cb1923b23febdf6
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-22 14:33:13 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+// a is a identifier and 7 is a literal(constant value)
+//Declaration of a variable.
+var a = 7;
+console.log(a);
+
+a = "Harry";
+console.log(a);
+
+// let 8harry = 7 //Not allowed it will throw an error
+// let var = 7 // throws an error
+//js is case sensitive
+
+// var,const,let difference.
+
+
+
+
+
+
