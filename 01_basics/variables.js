5f9370f1ed01a97a9af81aea738ee57516dddbea
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-22 16:58:41 +0000

    variables

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -11,6 +11,15 @@ console.log(a);
 //js is case sensitive
 
 // var,const,let difference.
+//var:used in ES6 , const:constant value in whole program , let: to declare block-scope variable.
+//var can be changed from any place. let can be used to create a block scope variable
+//In var updation and reclaration is allowed but in let you cant redeclare,only updation is possible.
+//******alt and press to create a cursor */
+console.log("Javascript tutorial 3 : var,const,and let");
+const author = "Harry";
+author=5; //throes an error.because constant cannot be changed 
+//const harry;//you have to initialize it.
+
 
 
 
