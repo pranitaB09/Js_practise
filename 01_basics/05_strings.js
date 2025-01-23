16ef22ebf8b661af75d8fad1b7a895f9da2477a2
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-23 17:45:07 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,41 @@
 const name = "hitesh"
 const repoCount = 50
 
-console.log(name + repoCount + "Value");
+//console.log(name + repoCount + "Value");//Not an appropriate method
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//Anothe wat to create string 
+const gameName = new String('hitesh-hc')
+
+//console.log(gameName[2]);
+//console.log(gameName.__proto__);//{} contains methods
+//console.log(gameName.length);//8
+//console.log(gameName.toUpperCase());//HITESHHC.But original value is not changed.
+//console.log(gameName.charAt(4)); // character at this position.and if index is greater means not available then empty space is shown  
+//console.log(gameName.indexOf('s')); //returns position of first occurences in string.and if character not present in string then -1 will retuned.
+
+const newString = gameName.substring(0, 4);// 0 is start position and 4 is end number.4th is excluded.
+//console.log(newString);
+
+const anotherString = gameName.slice(-8 , 4)//here we can give he negative values also.
+//console.log(anotherString);
+
+const newStringOne = "    hitesh   "
+console.log(newStringOne);
+console.log(newStringOne.trim());//to remove starting and ending spaces from string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
