6ef83e4554a5d7b9601b8099b54a21fcce8a4602
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-23 08:47:07 +0000

    primitiveandobjects

--- 01_basics/primitive_and_objects.js
+++ 01_basics/primitive_and_objects.js
@@ -1,12 +1,36 @@
-/**
- * N-Null
- * N-Number
- * S-Symbol
- * S-String
- * B-Boolean
- * B-Bigint
- * U-Undefined
- */
+// /**
+//  * N-Null
+//  * N-Number
+//  * S-Symbol
+//  * S-String
+//  * B-Boolean
+//  * B-Bigint
+//  * U-Undefined
+//  */
 //Datatype-means what type of data is strored in a variable like number,string
-//Object:key-value pairs 
+//Object:key-value pairs.key may be simple string,can be a number and value is any kind od datatype
+//There are 7 primitive datatype and non primitive datatype is object 
+let a = null;
+let b = 345;
+let c = true; //can also be false.
+let d = BigInt("567") + BigInt("3")
+let e = "Harry"
+let f = Symbol("I am a nice symbol")
+let g 
+console.log(a,b,c,d,e,f,g);
+console.log(typeof c);
+
+//objects-
+const iten = {
+    "Harry":true,
+    "Shubh": false,
+    "Lovish": 67,
+    "Rohan": undefined
+}
+console.log(item["Harry"]);
+
+
+
+
+
 
