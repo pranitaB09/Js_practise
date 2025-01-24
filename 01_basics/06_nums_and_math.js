c69c28e7be8e3230782babf05867ee5276a5d779
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-24 06:53:11 +0000

    num_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,23 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));//precision value after decimal
+
+const otherNumber = 1123.8966
+//console.log(otherNumber.toPrecision(3));//returns 1.12e+3
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString());//by default US standard 1,000,000
+//console.log(hundreds.toLocaleString('en-IN'));//generally in indian standard 10,00,000
+
+//********************Maths**************************/
+
+
+
+
