0e944478a991534c5173cc401b8541ccb3f1968e
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-24 13:29:22 +0000

    num_math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -25,11 +25,18 @@ const hundreds = 1000000
 //console.log(Math.floor(4.9));//take lower value
 //sqrt=no*no
 
-console.log(Math.min(4,3,6,8));//3
-console.log(Math.max(4,3,6,8));//8
+//console.log(Math.min(4,3,6,8));//3
+//console.log(Math.max(4,3,6,8));//8
 
-console.log(Math.random());//always between 0 to 1
+//console.log(Math.random()); // always between 0 to 1 means 0.1, 0.4, 0.5 like this
+//console.log((Math.random() * 10) + 1); // multiplied with 10 so that one place is moved.But if 0.01 then their is chance of 0 to display so to avoid it add 1 in it. 
+//console.log(Math.floor(Math.random() * 10) + 1);// math.floor to wrap the value and show the floor value.
 
+//sometimes you have to show the min and max values
+const min = 10
+const max = 20
+/*************************************************/
+console.log(Math.floor(Math.random()* (max - min + 1 )) + min )
 
 
 
