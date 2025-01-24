6a47405ea0b78f35379bc45df9bd156a5dba688f
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-24 10:09:07 +0000

    num_math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -18,6 +18,22 @@ const hundreds = 1000000
 
 //********************Maths**************************/
 
+//console.log(Math);//itself a object having properties
+//console.log(Math.abs(-4));//means -sign is ignoredor converted in + (4)
+//console.log(Math.round(4.4));//round off 4.8=5 if greater than 5 then choose bigger no.
+//console.log(Math.ceil(4.2));//take upper value
+//console.log(Math.floor(4.9));//take lower value
+//sqrt=no*no
+
+console.log(Math.min(4,3,6,8));//3
+console.log(Math.max(4,3,6,8));//8
+
+console.log(Math.random());//always between 0 to 1
+
+
+
+
+
 
 
 
