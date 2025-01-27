d9748bf61f25dc4a8949311682ed62b60baccca3
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-27 17:55:00 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+// FUNCTIONS : 
+
+function sayMyName(){
+console.log("H");
+console.log("I");
+console.log("T");
+console.log("E");
+console.log("S");
+console.log("H");
+}
+
+//sayMyName => reference(nothing displayed in o/p as it tells only that the function resides their.)
+//sayMyName() => execution.
+
+//sayMyName()
+
+function addTwoNumbers(number1,number1){
+console.log(number1 + number2 );
+}
+
+addTwoNumbers()
