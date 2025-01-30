24030fc6b86c5b4ca8f89b6e17a89b9fd9461bf2
pranitaB09 <pranitabirajdar09@gmail.com>
2025-01-30 18:48:39 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,36 @@
+//Switch => when there is a value and you have to check it with multiple values.inthat case switch statement is useful.
+
+//you have key and you check multiple values for that key.
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+// shift + alt + down arrow => for duplicate.
+
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
